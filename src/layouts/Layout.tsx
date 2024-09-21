0b43fa65caa9c808ff8eb4e4358dd0525f1b9f8b
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-09-21 15:57:41 +0330

    refactor: bottom navigation folder

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import BottomNavigation from "components/nav-bar/BottomNavigation";
+import BottomNavigation from "components/nav-bar/bottom-navigation/BottomNavigation";
 import { Outlet } from "react-router-dom";
 import { Container, OutletContainer } from "./layout.styles";
 
