7c014cae6a7821e821fbb1538bacbf145585bd3f
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-09-21 15:59:07 +0330

    refactor: file name

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,6 @@
 import BottomNavigation from "components/nav-bar/bottom-navigation/BottomNavigation";
 import { Outlet } from "react-router-dom";
-import { Container, OutletContainer } from "./layout.styles";
+import { Container, OutletContainer } from "./layout.style";
 
 export default function Layout() {
   return (
